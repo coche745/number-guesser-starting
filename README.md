1bf11bf89917ecc81784a4cd7915d566b250cc4e
Coche <70551980+coche745@users.noreply.github.com>
2021-04-04 21:34:42 -0500

    explanations of functions and readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Number Guesser
+## Table of Contents
++ General Info
++ Technologies
++ Features
++ Setup
++ Status
+## General Info
+This is a guessing game where a secret target number between 0 and 9 is generated and you and the computer guess a number and see who is closest to the target number. You can go as many rounds as you want to go, and each round the winner gets one point added to their score.
+## Technologies
++ Javascript
+## Features
++ Generates a random target number.
++ Takes input from the player for his guess.
++ Alerts the player if his guess is out of the range between 0 and 9.
++ Determines whether player's or computer's guess was closer to target.
++ Updates score by 1 each round to whoever was closer.
++ Updates the round number each round.
+## Setup
+To run this project, install it locally using git
++ git clone https://github.com/coche745/number-guesser-starting
++ open index.html file in browser, and play the game!
+# Status
+Project is: in progress
--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// generates a random target for the game
 const generateTarget = () => {
     target = Math.floor(Math.random() * 10);
     return target;
   }
 
+  // finds the difference between two numbers, will use to find difference between target and computer and human guesses
   const getAbsoluteDistance = (numOne, numTwo) => {
       return Math.abs(numOne - numTwo);
   }
 
+  // alerts the player if they have input a number that is out of range from 0 to 9.
   const alert = (guess) => {
       if (guess < 0 || guess > 9) {
           console.log('Number is out of range! Please pick a number between 0 and 9.');
@@ -22,6 +24,7 @@ const generateTarget = () => {
       }
   }
   
+  // returns true if human is closer, false if computer is closer.
   const compareGuesses = (human, computer, target) => {
     if (alert(human) === true) {
       return;
@@ -35,6 +38,7 @@ const generateTarget = () => {
     }
   }
   
+  // adds 1 point to either human or computer score depending on who's guess was closer.
   const updateScore = (winner) => {
     if (winner === 'human' || winner === true) {
       humanScore += 1;
@@ -43,16 +47,22 @@ const generateTarget = () => {
     }
   }
   
+  // adds 1 to the round.
   const advanceRound = () => {
     currentRoundNumber += 1;
   }
   
   target = generateTarget();
   console.log(target);
-  winner = compareGuesses(9, 0, target);
+
+  /* code to test functions, you can uncomment, and run this code in a terminal, but it will slightly mess up 
+  the score of the game if you play it on the browser, so make sure to comment it again */
+
+  /* winner = compareGuesses(9, 0, target);
   console.log(winner);
   updateScore(winner);
   console.log(humanScore);
-  console.log(computerScore);
+  console.log(computerScore); */
+
   advanceRound();
   console.log(currentRoundNumber);
